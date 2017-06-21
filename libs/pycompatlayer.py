bd5e35e8c1e9a9c1b126fa95887d7b711528aeb1
ale5000 <ale5000-git@users.noreply.github.com>
2017-06-21 02:58:22 +0200

    Updated compatibility layer

--- libs/pycompatlayer.py
+++ libs/pycompatlayer.py
@@ -1,19 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 """PyCompatLayer - Compatibility layer for Python.
 
-It make all versions of Python behaving as the latest version of Python 3.x.
-This will allow you to be compatible with all versions of Python without effort.
+It make all versions of Python behaving as the latest version of Python 3.
+It will allow to add compatibility with all versions of Python without effort.
 It is still under development, not all functions are supported.
 """
 
 import sys
 
-__version__ = "0.0.10"
+__version__ = "0.0.10-2"
 __author__ = "ale5000"
-__copyright__ = "Copyright (C) 2016, ale5000"
+__copyright__ = "Copyright (C) 2016-2017, ale5000"
 __license__ = "LGPLv3+"
 
 
+def set_utf8_default():
+    if sys.getdefaultencoding() != "utf-8":
+        try:
+            reload(sys)
+            sys.setdefaultencoding("utf-8")
+        except NameError:
+            pass
+
+
 def fix_base(fix_environ):
+    """Activate the base compatibility."""
     def _fix_android_environ():
         import os
 
@@ -21,9 +33,9 @@ def fix_base(fix_environ):
             os.environ["LD_LIBRARY_PATH"] = "."
 
         lib64_path = ""
-        lib32_path = os.pathsep + "/system/lib"
+        lib32_path = os.pathsep+"/system/lib"+os.pathsep+"/vendor/lib"
         if os.path.exists("/system/lib64"):
-            lib64_path = os.pathsep + "/system/lib64"
+            lib64_path = os.pathsep+"/system/lib64"+os.pathsep+"/vendor/lib64"
 
         os.environ["LD_LIBRARY_PATH"] += lib64_path + lib32_path
 
@@ -43,6 +55,7 @@ def fix_base(fix_environ):
 
 
 def fix_builtins(override_debug=False):
+    """Activate the builtins compatibility."""
     override_dict = {}
     orig_print = None
     used_print = None
@@ -59,7 +72,8 @@ def fix_builtins(override_debug=False):
     def _deprecated(*args, **kwargs):
         """Report the fact that the called function is deprecated."""
         import traceback
-        raise DeprecationWarning("the called function is deprecated => "+traceback.extract_stack(None, 2)[0][3])
+        raise DeprecationWarning("the called function is deprecated => " +
+                                 traceback.extract_stack(None, 2)[0][3])
 
     def _print_wrapper(*args, **kwargs):
         flush = kwargs.get("flush", False)
@@ -75,14 +89,16 @@ def fix_builtins(override_debug=False):
             if(key in opt):
                 opt[key] = kwargs[key]
             else:
-                raise TypeError("'"+key+"' is an invalid keyword argument for this function")
+                raise TypeError("'"+key+"' is an invalid keyword argument "
+                                "for this function")
         opt["file"].write(opt["sep"].join(str(val) for val in args)+opt["end"])
         if opt["flush"]:
             opt["file"].flush()
 
-    def _sorted(list):
-        list.sort()
-        return list
+    def _sorted(my_list):
+        my_list = list(my_list)
+        my_list.sort()
+        return my_list
 
     if builtins_dict.get(__name__, False):
         raise RuntimeError(__name__+" already loaded")
@@ -112,25 +128,28 @@ def fix_builtins(override_debug=False):
 
 
 def fix_subprocess(override_debug=False, override_exception=False):
+    """Activate the subprocess compatibility."""
     import subprocess
 
-    class _ExtendedCalledProcessError(subprocess.CalledProcessError):
+    class ExtendedCalledProcessError(subprocess.CalledProcessError):
+        """This exception is raised when a process run by check_call() or check_output() returns a non-zero exit status."""
         def __init__(self, returncode, cmd, output=None, stderr=None):
             try:
-                super(self.__class__, self).__init__(returncode=returncode, cmd=cmd, output=output, stderr=stderr)
+                super(ExtendedCalledProcessError, self).__init__(returncode=returncode, cmd=cmd, output=output, stderr=stderr)
             except TypeError:
                 try:
-                    super(self.__class__, self).__init__(returncode=returncode, cmd=cmd, output=output)
+                    super(ExtendedCalledProcessError, self).__init__(returncode=returncode, cmd=cmd, output=output)
                 except TypeError:
-                    super(self.__class__, self).__init__(returncode=returncode, cmd=cmd)
+                    super(ExtendedCalledProcessError, self).__init__(returncode=returncode, cmd=cmd)
                     self.output = output
                 self.stdout = output
                 self.stderr = stderr
 
-    def _check_output(*popenargs, **kwargs):
+    def _check_output(*args, **kwargs):
         if "stdout" in kwargs:
-            raise ValueError("stdout argument not allowed, it will be overridden.")
-        process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
+            raise ValueError("stdout argument not allowed, "
+                             "it will be overridden.")
+        process = subprocess.Popen(stdout=subprocess.PIPE, *args, **kwargs)
         stdout_data, __ = process.communicate()
         ret_code = process.poll()
         if ret_code is None:
@@ -138,17 +157,18 @@ def fix_subprocess(override_debug=False, override_exception=False):
         if ret_code:
             cmd = kwargs.get("args")
             if cmd is None:
-                cmd = popenargs[0]
-            raise _ExtendedCalledProcessError(returncode=ret_code, cmd=cmd, output=stdout_data)
+                cmd = args[0]
+            raise ExtendedCalledProcessError(returncode=ret_code, cmd=cmd, output=stdout_data)
         return stdout_data
 
     try:
-        from subprocess import check_output
-    except ImportError:
+        subprocess.check_output
+    except AttributeError:
         subprocess.check_output = _check_output
 
 
 def fix_all(override_debug=False, override_all=False):
+    """Activate the full compatibility."""
     fix_base(True)
     fix_builtins(override_debug)
     fix_subprocess(override_debug, override_all)
